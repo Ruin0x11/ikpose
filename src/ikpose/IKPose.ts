e245ecbbd765ce4f73858ab19d88dd12ff0c4213
Ruin0x11 <ipickering2@gmail.com>
2021-04-05 01:57:28 -0700

    Use three.js instead of babylon.js

---
+++ src/ikpose/IKPose.ts
@@ -0,0 +1,113 @@
+import * as THREE from "three";
+import * as dat from "dat.gui";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { TransformControls } from 'three/examples/jsm/controls/TransformControls.js';
+
+export class IKPose {
+    public renderer: THREE.WebGLRenderer;
+    public camera: THREE.PerspectiveCamera;
+    public scene: THREE.Scene;
+
+    public transformControl: TransformControls;
+
+    public params: any = {
+        uniform: false
+    };
+
+    constructor() {
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.shadowMap.enabled = true
+        document.body.appendChild(this.renderer.domElement);
+
+        this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color(0xf0f0f0);
+
+        this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000);
+        this.camera.position.set(0, 250, 1000);
+        this.scene.add(this.camera);
+
+        this.addLight();
+        this.addPlane();
+        this.addGrid();
+
+        this.bindControls();
+
+        this.openGUI();
+
+        this.render();
+    }
+
+    private addLight() {
+        this.scene.add(new THREE.AmbientLight(0xf0f0f0));
+        const light = new THREE.SpotLight(0xffffff, 1.5);
+        light.position.set(0, 1500, 200);
+        light.angle = Math.PI * 0.2;
+        light.castShadow = true;
+        light.shadow.camera.near = 200;
+        light.shadow.camera.far = 2000;
+        light.shadow.bias = -0.000222;
+        light.shadow.mapSize.width = 1024;
+        light.shadow.mapSize.height = 1024;
+        this.scene.add(light);
+    }
+
+    private addPlane() {
+        const planeGeometry = new THREE.PlaneGeometry(2000, 2000);
+        planeGeometry.rotateX(-Math.PI / 2);
+        const planeMaterial = new THREE.ShadowMaterial({ opacity: 0.2 });
+
+        const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+        plane.position.y = - 200;
+        plane.receiveShadow = true;
+        this.scene.add(plane);
+    }
+
+    private addGrid() {
+        const helper = new THREE.GridHelper(2000, 100);
+        helper.position.y = -199;
+        const material: THREE.Material = helper.material as THREE.Material
+        material.opacity = 0.25;
+        material.transparent = true;
+        this.scene.add(helper);
+    }
+
+    private bindControls() {
+        // Controls
+        const controls = new OrbitControls(this.camera, this.renderer.domElement);
+        // controls.damping = 0.2;
+        controls.addEventListener('change', () => this.render());
+
+        this.transformControl = new TransformControls(this.camera, this.renderer.domElement);
+        this.transformControl.addEventListener('change', () => this.render());
+        this.transformControl.addEventListener('dragging-changed', function(event) {
+            controls.enabled = !event.value;
+        });
+        this.scene.add(this.transformControl);
+
+        document.addEventListener('pointerdown', this.onPointerDown);
+        document.addEventListener('pointerup', this.onPointerUp);
+        document.addEventListener('pointermove', this.onPointerMove);
+    }
+
+    private openGUI() {
+        const gui = new dat.GUI();
+
+        gui.add(this.params, 'uniform');
+        gui.open();
+    }
+
+    private onPointerDown(event: PointerEvent) {
+    }
+
+    private onPointerUp(event: PointerEvent) {
+    }
+
+    private onPointerMove(event: PointerEvent) {
+    }
+
+    private render() {
+        this.renderer.render(this.scene, this.camera);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,3 @@
-import "@babylonjs/core/Loading/loadingScreen";
-import "@babylonjs/loaders";
-import "@babylonjs/core/Loading/Plugins/babylonFileLoader"
-import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera"
-import { Engine } from "@babylonjs/core/Engines/engine"
-import { Vector3 } from "@babylonjs/core/Maths/math.vector"
+import { IKPose } from "./ikpose/IKPose";
 
-import { PoserScene } from "./PoserScene"
-import { ArcRotateCameraMouseWheelInput } from "@babylonjs/core";
-
-const view = document.getElementById("view") as HTMLCanvasElement
-const engine = new Engine(view, true)
-const scene = new PoserScene(engine)
-
-const camera = new ArcRotateCamera(
-    "camera",
-    Math.PI / 2,
-    Math.PI / 3.2,
-    200,
-    Vector3.Zero(),
-    scene)
-
-const mousewheel = camera.inputs.attached.mousewheel as ArcRotateCameraMouseWheelInput;
-mousewheel.wheelPrecision = 100;
-
-camera.attachControl(view)
-
-engine.runRenderLoop(() => {
-    scene.render();
-})
-
-window.addEventListener("resize", function() {
-    engine.resize();
-});
+const ikpose = new IKPose();
