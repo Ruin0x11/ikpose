e8b8a9cc03cdfc20262a66ce561391dadcfda3dd
Ruin0x11 <ipickering2@gmail.com>
2021-04-09 21:44:34 -0700

    Fix model path

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { IKPose } from "./ikpose/IKPose";
 import { Vector3 } from "three";
 
 const ikpose = new IKPose();
-ikpose.loadModel("/assets/models/three-vrm-girl.vrm", new Vector3(-0.5, 0, 0))
-ikpose.loadModel("/assets/models/AliciaSolid.vrm", new Vector3(0.5, 0, 0))
+ikpose.loadModel("assets/models/three-vrm-girl.vrm", new Vector3(-0.5, 0, 0))
+ikpose.loadModel("assets/models/AliciaSolid.vrm", new Vector3(0.5, 0, 0))
